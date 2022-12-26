fa5d1a47caebd0f15f990a4164e9dfe86162874f
Bahman <abbahmanyar@gmail.com>
2022-12-26 23:05:53 +0330

    Add web app

---
+++ webapp/backend/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import os
+from flask import Flask, request, redirect, Response, jsonify, send_from_directory
+from markupsafe import escape
+from flask_cors import CORS, cross_origin
+from tensorflow import keras
+
+app = Flask(__name__)
+CORS(app)
+
+model_filepath = 'model/final'
+model = keras.models.load_model(model_filepath)
+
+@app.route('/predict', methods=['GET'])
+@cross_origin()
+def upload_media():
+    text = request.values.get('text')
+    if text is None or len(text) == 0:
+        return "", 400
+    
+    return jsonify({
+        "status": 0,
+        "msg": "success",
+        "result": str(model.predict([text])[0][0])
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
---
+++ webapp/frontend/nlp-app/src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    function analyze() {
+        console.log("bla bla bla")
+    }
+</script>
+
+<html lang="en">
+    <head>
+        <title>
+            NLP model test
+        </title>
+        <link rel="stylesheet" href="./main.css">
+        
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="vbox margin_lr">
+              <div class="margin_top_7"></div>
+              <form class="center vbox">
+              <label for="text_input" class="form_label">
+                  Enter sample text:
+                </label>
+                <input class="form_input" type="text" name="text_input" />
+              </form>
+              <button class="s_btn btn0" on:click={analyze}>Analyse!</button>
+            </div>
+          </div>
+    
+    </body>
+</html>
